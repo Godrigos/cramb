b18353ea7f7b5d6cd6d6ab0f745e2019740d6fd9
Rodrigo Aluizio <r.aluizio@gmail.com>
2018-09-07 07:36:58 -0300

    Retrieve old results.

--- job_show.py
+++ job_show.py
@@ -1,17 +1,15 @@
 #!/usr/bin/env python
 
-from tkinter import *
-
 
 def job_show(self, job, messages=True):
+    msg = ""
     """ A debugging method to dump some of the content of this object to text_box entry """
-
     if not job.jobHandle and job.commandline:
-        self.text_box.config(state=NORMAL)
-        self.text_box.insert(END, "Submission validated.  Commandline is: " + job.commandline + '\n', "cool")
-        self.text_box.config(state=DISABLED)
+        string = str("Submission validated.  Commandline is: " + job.commandline + '\n')
+        return string
 
     string = str("Job=" + job.jobHandle)
+
     if job.terminalStage:
         if job.failed:
             string += str(", failed at stage " + job.jobStage + '\n')
@@ -19,21 +17,15 @@ def job_show(self, job, messages=True):
             string += str(", finished, results are at " + job.resultsUrl + '\n')
     else:
         string += str(", not finished, stage=" + job.jobStage + '\n')
-    self.text_box.config(state=NORMAL)
-    self.text_box.insert(END, string, "cool")
-    self.text_box.config(state=DISABLED)
+
     if messages:
         for m in job.messages:
-            self.text_box.config(state=NORMAL)
-            self.text_box.insert(END, "\t" + m + "\n", "cool")
-            self.text_box.config(state=DISABLED)
+            msg += "\t" + m + "\n"
         if job.metadata:
-            self.text_box.config(state=NORMAL)
-            self.text_box.insert(END, "Metadata:\n", "cool")
+            msg += "Metadata:\n"
             for key, value in job.metadata.items():
-                self.text_box.insert(END, "\t" + key + ': ' + value + '\n', "cool")
-            self.text_box.config(state=DISABLED)
+                msg += "\t" + key + ': ' + value + '\n'
         else:
-            self.text_box.config(state=NORMAL)
-            self.text_box.insert(END, "There is no metadata.\n", "cool")
-            self.text_box.config(state=DISABLED)
+            msg = "There is no metadata.\n"
+
+    return string, msg
