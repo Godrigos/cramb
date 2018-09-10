1ec68d96d0e9e37984974e148bfc0cf4f30f0847
rodrigo <r.aluizio@gmail.com>
2018-09-10 13:06:49 -0300

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='cramb',
+    version='2.0',
+    packages=[''],
+    url='https://github.com/Godrigos/cramb',
+    license='GPL-3.0',
+    author='Rodrigo Aluizio',
+    author_email='',
+    description='CIPRES Rest API - MrBayes Client'
+)
