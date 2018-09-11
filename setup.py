114ffde0f6c047cd73a82c135ef78c95aa9c26b8
rodrigo <r.aluizio@gmail.com>
2018-09-11 12:59:49 -0300

    Finished setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,21 @@
-from setuptools import setup
+import setuptools
 
-setup(
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name='cramb',
     version='2.0.1',
-    packages=[''],
     url='https://github.com/Godrigos/cramb',
     license='GPL-3.0',
     author='Rodrigo Aluizio',
     author_email='',
-    description='CIPRES Rest API - MrBayes Client'
+    description='CIPRES Rest API - MrBayes Client',
+    long_description=long_description,
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GPL-3.0",
+        "Operating System :: OS Independent",
+    ],
 )
